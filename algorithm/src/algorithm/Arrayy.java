b324cb6618069729372bf7e41f1646e06efc2f92
NL.A <nl.a@mit.com.mm>
2025-09-18 23:35:11 +0630

    array DSA

--- algorithm/src/algorithm/Arrayy.java
+++ algorithm/src/algorithm/Arrayy.java
@@ -4,6 +4,14 @@ public class Arrayy {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int[] num= {3,45,2,6,7,34,66};
+		int max=num[0];
+		for(int i=0;i<num.length;i++) {
+			if(num[i]>max) {
+				max=num[i];
+			}
+		}
+		System.out.println("%s is Maximum Number".formatted(max));
 
 	}
 
